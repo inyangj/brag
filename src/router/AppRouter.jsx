b1c5af8657adeb83f8f536d8bb3f6a0bff705a0f
inyangj <inyangjohnson01@gmail.com>
2024-02-05 16:11:38 +0100

    layout

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
-import Forgotpassword from "../pages/Forgotpassword ";
+import Forgotpassword from "../pages/Forgotpassword";
 import Createpassword from "../pages/Createpassword";
 import Home from "../pages/Home";
 import Layout from "../pages/Layout";
+import Business from "../pages/Business";
 
 
 
@@ -19,7 +20,9 @@ const AppRouter = () => {
         <Route path={`/signup`} element={<Signup />} />
         <Route path={`/forgotpassword`} element={<Forgotpassword />} />
         <Route path={`/createpassword`} element={<Createpassword />} />
-        <Route path={`/business`} element={<Layout />} />
+        <Route path={`/business`} element={<Layout />} >
+          <Route path={`/business`} element={<Business />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
